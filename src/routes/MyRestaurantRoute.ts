2cde7ef60b1ad5546f5b19a9792479eb1b0f2f61
Mohd Sultan <sultan.mohd2023@gmail.com>
2024-04-23 09:42:46 +0530

    Done till Create restaurant API

---
+++ src/routes/MyRestaurantRoute.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import multer from "multer";
+import MyRestaurantController from "../controllers/MyRestaurantController";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyRestaurantRequest } from "../middleware/validation";
+
+const router = express.Router();
+
+const storage = multer.memoryStorage();
+const upload = multer({
+  storage: storage,
+  limits: {
+    fileSize: 5 * 1024 * 1024, //5mb
+  },
+});
+
+// /api/my/restaurant
+router.post(
+  "/",
+  upload.single("imageFile"),
+  validateMyRestaurantRequest,
+  jwtCheck,
+  jwtParse,
+  MyRestaurantController.createMyRestaurant
+);
+
+export default router;
